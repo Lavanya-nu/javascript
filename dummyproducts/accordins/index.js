5fd0efe16a08fdc25d7e706455f31702b7fd3a3f
Lavanya-nu <152372410+Lavanya-nu@users.noreply.github.com>
2024-02-07 10:19:41 +0530

    Add files via upload

---
+++ dummyproducts/accordins/index.js
@@ -0,0 +1,20 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
---
+++ dummyproducts/demo.js
@@ -0,0 +1,46 @@
+const mainDiv = document.getElementById('results');
+fetchData('https://dummyjson.com/products');
+let count=0;
+function fetchData (url)
+{
+    fetch(url)
+        .then((res) => res.json() )
+        .then((data)=>displayData(data.products))
+        .catch(function (error)
+        {
+            console.error(error)
+        })
+    
+}
+function priceUpdate (p)
+{
+    console.log(p.price)
+}
+const incr= ()=>
+{
+    count++;
+    document.querySelector("#cart>span").textContent = count
+}
+const displayData =(products)=>
+{
+    products.forEach(function (pro)
+    {
+        const product = document.createElement('div')
+        product.classList.add('product')
+        const img = document.createElement('img');
+        img.setAttribute('src', pro.images[0])
+        img.alt = "image"
+        const title = document.createElement('h2')
+        title.textContent = pro.title
+        const price = document.createElement('p')
+        price.textContent = "Price: $" + pro.price
+        const category = document.createElement('p')
+        category.textContent = "Category:" + pro.category;
+        const btn = document.createElement('button')
+        btn.textContent = "Add to Cart"
+        btn.addEventListener('click', incr)
+        btn.onclick=priceUpdate(pro)
+        product.append(img, title, category, price, btn);
+        mainDiv.appendChild(product)
+    });
+}
